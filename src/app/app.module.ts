2d4905d967f5195eb491e6a87105f24e2738aa61
Christian Bremer <christian.bremer@cstx.de>
2019-05-11 10:44:57 +0200

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,29 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {OAuthModule} from 'angular-oauth2-oidc';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import { OpeningComponent } from './opening/opening.component';
-import { AuthComponent } from './auth/auth.component';
-import { WorkbenchComponent } from './workbench/workbench.component';
+import {AuthComponent} from './auth/auth.component';
+import {OpeningComponent} from './opening/opening.component';
+import {WorkbenchComponent} from './workbench/workbench.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    OpeningComponent,
     AuthComponent,
+    OpeningComponent,
     WorkbenchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule,
+    FontAwesomeModule,
+    AppRoutingModule,
     HttpClientModule,
     OAuthModule.forRoot()
   ],
